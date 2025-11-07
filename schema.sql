dc7ec8c13b731fc93776ee87a99bb2924b160cc7
Joona Koiranen <jkoiranen@outlook.com>
2025-11-07 15:24:14 +0200

    Add registration, login and logout.

---
+++ schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE collection_methods (id INTEGER PRIMARY KEY, name TEXT UNIQUE NOT NULL);
+CREATE TABLE data_formats (id INTEGER PRIMARY KEY, name TEXT UNIQUE NOT NULL);
+CREATE TABLE scientific_fields (id INTEGER PRIMARY KEY, name TEXT UNIQUE NOT NULL);
+CREATE TABLE datasets(
+  id INT,
+  name TEXT,
+  description TEXT,
+  data_formats_id INT,
+  collection_method_id INT,
+  scientific_field_id INT,
+  year INT,
+  user_id INT,
+  request TEXT
+);
+CREATE TABLE IF NOT EXISTS "users" (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE,
+  password_hash TEXT
+);
+CREATE TABLE sqlite_sequence(name,seq);
